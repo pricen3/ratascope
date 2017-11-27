9c5cefcd4d0b2c04a89d3735b86aeaaa02551e66
bpeters1954 <bpeters1954@gmail.com>
2017-11-27 00:13:23 -0800

    Update Cage.java

--- Cage.java
+++ Cage.java
@@ -1,4 +1,43 @@
-public class Cage {
+import java.util.*;
 
+public class Cage {
+   private int cageID;
+   private int lightOnTime;
+   private int lightOffTime;
+   private Experiment exp;
+   private Mouse inhabitant;
+   private Pi p;
+   private boolean expOngoing;
+   
+   public Cage(int cage, Experiment exper, Mouse mouse, Pi pi, boolean going, int lightOn, int lightOff){
+      cageID = cage;
+      exp = exper;
+      inhabitant = mouse;
+      p = pi;
+      expOngoing = going;
+      lightOnTime = lightOn;
+      lightOffTime = lightOff;
+   }
+   
+   public int getID(){
+      return cageID;
+   }
+   
+   public void setInhabitant(Mouse mouse){
+      inhabitant = mouse;
+   }
+   
+   public Mouse getInhabitant(){
+      return inhabitant;
+   }
+   
+   public void process(String fileName){
+   
+   }
+   
+   public void setLightTimes(int on, int off){
+      lightOnTime = on;
+      lightOffTime = off;
+   }
    
 }
