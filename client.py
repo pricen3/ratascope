c50b5b1d2cf5ac5917b07aa586139d452063c1eb
Nolan Price <pricen3@wwu.edu>
2018-02-18 14:31:23 -0800

    ui communicates with pi now :)

--- client.py
+++ client.py
@@ -10,6 +10,27 @@ import signal
 ###for our project. If not, just delete it
 ###the main is still needed
 
+def parse_command_line_args():
+    """ Use argparse to return command line arguments
+    as a namespace object.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-ip',
+        help='IP address of cage',
+        metavar='ip',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '-s',
+        help='Comand string to be sent',
+        metavar='s',
+        type=str,
+        required=True
+    )
+    return parser.parse_args()
+
 def send(connection, message):
     """send(connction, message) function"""
     mes = message.encode('ascii')
@@ -25,19 +46,24 @@ def recv(connection):
     except TimeoutError:
         print('timeoutError ocurred')
 
-def client(ip):
+def client(ip, send_string):
     """Client function"""
     s = socket.socket()
-    s.connect((ip, 3679))
+    s.connect((ip, 3456)) #3679
     cont = 0
     while(cont == 0):
         #message = recv(s)
+        #code = recv(s)
+        #print(code)
+        send(s, send_string)
         code = recv(s)
-        print(code)
+        if code == "Y":
+            cont += 1
     s.close
 
 def main():
-    client('140.160.139.121')
+    args = parse_command_line_args()
+    client(args.ip, args.s)
 
 if __name__ == '__main__':
     main()
